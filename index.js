fd5da0dfa2adda408d8680f201115632a9341d6b
Jackie <jye0325@gmail.com>
2022-05-20 01:12:03 -0400

    commit tree 5ad4f4b08239b54c64dc38a1e8d72f7c69be22cb

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then((res) => res.json())
+        .then((data) => document.body.append(data.id))
+        .catch((error) => document.body.append(error.message))
+
+}
+
+
+
+
+///////////////////////////////////////////////////////////
+// EXAMPLE CODE FOR FETCH FOR http://localhost:3000/dogs //
+///////////////////////////////////////////////////////////
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// }
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     })
+
+
